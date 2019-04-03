05c1988600ad4c3e3304d54bd371cea78690dad0
masturiri <2720601448@qq.com>
2019-04-03 15:04:20 +0800

    第一次提交

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var signRouter = require('./routes/sign');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/sign', signRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
